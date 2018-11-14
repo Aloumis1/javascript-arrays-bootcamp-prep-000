fb554ec5225bc8709113f178172a17046a6cebb0
Achilles P. Loumis <achilleas.loumis@gmail.com>
2018-11-14 21:13:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...arrays];
 }
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
